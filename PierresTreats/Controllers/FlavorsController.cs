835152f8bcef11066e7e9ed956a7c21446308204
Brian Scherner <brianscherner@gmail.com>
2023-12-14 15:39:05 -0800

    Add Index view for Flavors w/ routes + blank Treats controller file.

---
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using PierresTreats.ViewModels;
+
+namespace PierresTreats.Controllers
+{
+  public class FlavorsController : Controller
+  {
+    private readonly PierresTreatsContext _db;
+    public FlavorsController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Flavor> model = _db.Flavors.ToList();
+      return View(model);
+    }
+  }
+}
