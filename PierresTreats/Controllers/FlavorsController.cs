28c1875b08825cb20a0bb6cd37ec6f6b9823f2ce
Brian Scherner <brianscherner@gmail.com>
2023-12-15 13:58:02 -0800

    Add functionality to edit a flavor w/ authorization.

--- PierresTreats/Controllers/FlavorsController.cs
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -84,5 +84,43 @@ namespace PierresTreats.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    [Authorize]
+    public ActionResult Edit(int id)
+    {
+      Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Flavor flavor)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(flavor);
+      }
+      else
+      {
+        _db.Flavors.Update(flavor);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+    }
+
+    // [Authorize]
+    // public ActionResult Delete(int id)
+    // {
+    //   Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+    //   return View(thisTreat);
+    // }
+
+    // [HttpPost, ActionName("Delete")]
+    // public ActionResult DeleteConfirmed(int id)
+    // {
+    //   Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+    //   _db.Treats.Remove(thisTreat);
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
   }
 }
